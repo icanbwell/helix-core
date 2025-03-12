117e3c417be1b113aafa979953be6251763a80b7
Imran Qureshi <imranq2@hotmail.com>
2025-03-12 16:31:30 -0700

    get all tests working

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import os
 from typing import Generator
 
 import boto3
@@ -5,6 +6,16 @@ import pytest
 from _pytest.fixtures import FixtureFunctionMarker
 from botocore.client import BaseClient
 from moto import mock_aws
+
+
+@pytest.fixture(scope="function")
+def aws_credentials() -> None:
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_REGION"] = "us-east-1"
 
 
 @pytest.fixture(scope="function")
